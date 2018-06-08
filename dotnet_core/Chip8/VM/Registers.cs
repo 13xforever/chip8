7b3f1c8ed136010e2521e26cfba4d2251a272e76
13xforever <ilya_veselov@kontur.ru>
2018-06-08 21:39:57 +0500

    make some things readonly

--- dotnet_core/Chip8/VM/Registers.cs
+++ dotnet_core/Chip8/VM/Registers.cs
@@ -2,7 +2,7 @@
 {
     public class Registers
     {
-        public byte[] VR = new byte[16];
+        public readonly byte[] VR = new byte[16];
         public ref byte VF => ref VR[15];
         public ushort IR;
         public ushort PC = 0x0200;
--- dotnet_core/Chip8/VM/VM.cs
+++ dotnet_core/Chip8/VM/VM.cs
@@ -4,10 +4,10 @@ namespace Chip8VM
 {
     public class VM
     {
-        public Registers Registers = new Registers();
-        public byte[] Stack = new byte[64];
-        public ulong[] VideoBuffer = new ulong[32];
-        public byte[] Ram = new byte[4096];
+        public readonly Registers Registers = new Registers();
+        public readonly byte[] Stack = new byte[64];
+        public readonly ulong[] VideoBuffer = new ulong[32];
+        public readonly byte[] Ram = new byte[4096];
 
         public VM()
         {
